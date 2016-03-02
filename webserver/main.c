8ad61ebdc8b5df725c9e90e70cf196e2951fe9a2
Ouvry Ludovic <ludovic.ouvry@aliceadsl.fr>
2016-03-02 16:12:27 +0100

    en cour de la vérif du header, 5.2.2

--- webserver/main.c
+++ webserver/main.c
@@ -13,50 +13,85 @@ int main(void){
 	int socket_serveur = creer_serveur(8080);
 	initialiser_signaux();
 	while(1){
-		int socket_client ;
-		socket_client = accept ( socket_serveur , NULL , NULL );
-		if ( socket_client == -1){
-			perror ( " accept " );
-			break;
-		/* traitement d ’ erreur */
-		}
-		if(fork()==0){
-			/* On peut maintenant dialoguer avec le client */
-
-			//const char * message_bienvenue = "Ah, Bonjour , bienvenue sur mon serveur \n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n je suis un très long messageeeeeeeeeeeeeeeee \n" ;
-			//sleep(1);
-			
-			//write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-					
-			FILE *lireDonneClient=fdopen(socket_client,"w+"); // sert aussi pour lui envoyée les données 
-			if(lireDonneClient==NULL){
-				perror("fdopen");
+			int socket_client ;
+			socket_client = accept ( socket_serveur , NULL , NULL );
+			if ( socket_client == -1){
+				perror ( " accept " );
 				break;
-				/*traitement d'erreur*/
+			/* traitement d ’ erreur */
 			}
-		
-			while(1){
-				char buf[1024];
-				// seconde chaine pour la concaténation.
+			if(fork()==0){
+				/* On peut maintenant dialoguer avec le client */
+
+				//const char * message_bienvenue = "Ah, Bonjour , bienvenue sur mon serveur \n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n je suis un très long messageeeeeeeeeeeeeeeee \n" ;
+				//sleep(1);
 				
-		
-				char* get = fgets(buf,1024,lireDonneClient);
-				//char buf2[1036]="<DualDev>";
-				if(get==NULL){
-					perror("déconnexion");
+				//write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+						
+				FILE *lireDonneClient=fdopen(socket_client,"w+"); // sert aussi pour lui envoyée les données 
+				if(lireDonneClient==NULL){
+					perror("fdopen");
 					break;
-					/*traitement d'erreur ou deco, voir man fgets return Value*/ 
+					/*traitement d'erreur*/
 				}
-				//concaténation
-				//strcat(buf2,buf);
-
+				int premiereLigne=1; // teste si c'est la première ligne.
+				int ligne1Valide=1; //boolean
+				while(1){
+					char buf[1024];
+				
 			
-				//fprintf(lireDonneClient,buf2,strlen(buf2));
-				printf("%s",buf);
+					char* get = fgets(buf,1024,lireDonneClient);
+					
+					if(get==NULL){
+						perror("déconnexion");
+						break;
+						/*traitement d'erreur ou deco, voir man fgets return Value*/ 
+					}
+					int nbMots=0;
+					if(premiereLigne){
+						char* copiechaine=strdup(buf);
+						char* sto;
+						sto=strtok(copiechaine," "); //fonction qui se rapelle elle même voir le man
 
+						while(sto!=NULL){
+							printf("%s \n",sto);
+							nbMots++;
+							if(nbMots==1){
+								if(0!=strncmp(sto,"GET",3)){
+									ligne1Valide=0;
+								}
+							}
+							
+							if(nbMots==3){
+								if(0!=strncmp(sto,"HTTP/",5)){
+									ligne1Valide=0;
+								}
+								if(strlen(sto)>=8){
+									if(!(sto[6]=='1' && (sto[7]=='0' || sto[7]=='1'))){
+										ligne1Valide=0;
+									}
+								}else{
+									ligne1Valide=0;
+								}
+							}
+							sto=strtok(NULL," ");
+							
+						}
+						if(nbMots!=3){
+							ligne1Valide=0;
+
+						}
+					
+					premiereLigne=0;
+					printf("ligne valide:%d",ligne1Valide);
+				}
+				printf("%s\n",buf);
+					
 			}
-			exit(0);
+			printf("fin\n");
+				exit(0);
 		}
+		
 	}
-	return 0;
-} 
+return 0;
+}
