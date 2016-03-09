8d4bf7160013fa207f46c3e99c3511cdc2aadf41
Alexandre Courand <a.courand@hotmail.fr>
2016-03-09 15:36:53 +0100

    soucis

--- webserver/main.c
+++ webserver/main.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 
 int main(void){
+
 	int socket_serveur = creer_serveur(8080);
 	initialiser_signaux();
 	char* erreur400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
@@ -22,9 +23,11 @@ int main(void){
 			/* traitement d ’ erreur */
 			}
 			if(fork()==0){
+
+			
 				/* On peut maintenant dialoguer avec le client */
 
-				//const char * message_bienvenue = "Ah, Bonjour , bienvenue sur mon serveur \n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n je suis un très long messageeeeeeeeeeeeeeeee \n" ;
+				//const char * message_bienvenue = "Ah, Bonjour , bienvenue sur mon serveur \n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n je suis un très 					long messageeeeeeeeeeeeeeeee \n" ;
 				//sleep(1);
 				
 				//write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
@@ -35,7 +38,7 @@ int main(void){
 					break;
 					/*traitement d'erreur*/
 				}
-				int premiereLigne=1; // teste si c'est la première ligne.
+				int premiereLigne=0; // teste si c'est la première ligne.
 				int ligne1Valide=1; //boolean
 				while(1){
 					char buf[1024];
@@ -45,45 +48,45 @@ int main(void){
 					
 					if(get==NULL){
 						perror("déconnexion");
-						break;
+						exit(1);
 						/*traitement d'erreur ou deco, voir man fgets return Value*/ 
 					}
 					int nbMots=0;
-					if(premiereLigne){
+					if(premiereLigne==1){
 						char* copiechaine=strdup(buf);
 						char* sto;
 						sto=strtok(copiechaine," "); //fonction qui se rapelle elle même voir le man
 						if(sto==NULL){
 							perror("strtok");
-							break;
+							exit(1);
 						}
 						while(sto!=NULL){
 							//printf("%s \n",sto);
 							nbMots++;
 							if(nbMots==1){
 								if(0!=strncmp(sto,"GET",3)){
-									printf("%s\n",erreur400);
+									fprintf(lireDonneClient,erreur400,strlen(erreur400));
 									ligne1Valide=0;
-									break;
+									exit(1);
 								}
 							}
 							
 							if(nbMots==3){
 								if(0!=strncmp(sto,"HTTP/",5)){
 									ligne1Valide=0;
-									printf("%s\n",erreur400);
-									break;
+									fprintf(lireDonneClient,erreur400,strlen(erreur400));
+									exit(1);
 								}
 								else if(strlen(sto)>=8){
 									if(!(sto[5]=='1' && (sto[7]=='0' || sto[7]=='1'))){
 										ligne1Valide=0;
-										printf("%s\n",erreur400);
-										break;
+										fprintf(lireDonneClient,erreur400,strlen(erreur400));
+										exit(1);
 									}
 								}else{
 									ligne1Valide=0;
-									printf("%s\n",erreur400);
-									break;
+									fprintf(lireDonneClient,erreur400,strlen(erreur400));
+									exit(1);
 								}
 							}
 							sto=strtok(NULL," ");
@@ -91,20 +94,24 @@ int main(void){
 						}
 						if(nbMots!=3 && ligne1Valide==1){
 							ligne1Valide=0;
-							printf("%s\n",erreur400);
+							fprintf(lireDonneClient,erreur400,strlen(erreur400));
 						}
 					
 					premiereLigne=0;
 					printf("ligne valide:%d\n",ligne1Valide);
-				}
-				if(strcmp(buf,"\r\n")==0||strcmp(buf,"\n")){ //début des ligne à ne pas ignorer, debugage
-					printf("%s\n",buf);
-				}
+					}
+					if((strcmp(buf,"\n")==0 || strcmp(buf,"\r\n")==0)){
+						premiereLigne = 1;
+					}
+					if(strcmp(buf,"\r\n")==0||strcmp(buf,"\n")){ //début des ligne à ne pas ignorer, debugage
+						printf("%s\n",buf);
+					}
 					
 			}
 			printf("fin\n");
-				exit(0);
+			exit(0);
 		}
+		close(socket_client);
 		
 	}
 return 0;
