42b6694d05e56e5f1db1b6431d57ae39b6fa78b3
Ouvry Ludovic <ludovic.ouvry@aliceadsl.fr>
2016-03-31 19:27:11 +0200

    tout nettoyé

--- webserver/client.h
+++ webserver/client.h
@@ -1,5 +1,7 @@
 # ifndef CLIENT_H
 # define CLIENT_H
+#include <stdio.h>
+
 char *fgets_or_exit(char *buffer, int size, FILE *stream);
 
 
@@ -19,6 +21,6 @@ int parse_http_request ( const char * request_line , http_request * request );
 void afficheHTTPrequest(http_request request);
 void skip_headers(FILE* client);
 void send_status(FILE* client, int code, const char* reason_phrase);
-void send_response(FILE *client , int code , const char *reason_phrase , const char *message_body);
+void send_response(FILE *client , int code , const char *reason_phrase , const char *message_body); 
 
 # endif
--- webserver/main.c
+++ webserver/main.c
@@ -1,69 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include "socket.h"
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
+
 #include "client.h"
+#include "serveur.h"
+#include "socket.h"
 
 int main(void){
 
-	int socket_serveur = creer_serveur(8080);
+	
 	initialiser_signaux();
+	serveur();
 
-
-	while(1){
-			int socket_client ;
-			socket_client = accept ( socket_serveur , NULL , NULL );
-			if ( socket_client == -1){
-				perror ( " accept " );
-				break;
-		
-			}
-			if(fork()==0){
-
-			
-						
-				FILE *lireDonneClient=fdopen(socket_client,"w+"); // sert aussi pour lui envoyée les données 
-				if(lireDonneClient==NULL){
-					perror("fdopen");
-					break;
-					/*traitement d'erreur*/
-				}
-				
-		
-					char buf[1024];
-					char *repClient=fgets_or_exit(buf,1024,lireDonneClient);
-					printf("rep client: %s\n",repClient );
-
-					http_request request; 
-					int resultat_requete=parse_http_request(repClient,&request);
-					printf("résultat requête:%d\n",resultat_requete);
-					 afficheHTTPrequest(request);
-					skip_headers(lireDonneClient);
-					if (resultat_requete==0){
-						send_response(lireDonneClient , 400, "Bad Request", "Bad request\r\n");
-					}else if (request.method == HTTP_UNSUPPORTED){
-						send_response(lireDonneClient , 405, "Method Not Allowed", "Method Not Allowed\r\n");
-					}else if (strcmp(request.url, "/") == 0){
-						send_response(lireDonneClient , 200, "OK", "Bienvenue");
-						printf("test\n");
-					}else{
-						send_response(lireDonneClient , 404, "Not Found", "Not Found\r\n");
-					}
-					exit(0);
-
-
-					
-			
-		}
-		close(socket_client);
-		
-	}
+	
 return 0;
 }
 
